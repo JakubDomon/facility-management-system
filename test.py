27e2ba37a29248e4b18b0709fd812ad7e1a848a4
Jakub Domoń <70952494+JakubDomon@users.noreply.github.com>
2022-11-05 18:32:21 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from opcua import Client
-
-clients = []
-
-clients.append(Client('opc.tcp://192.168.0.55:4840'))
-clients.append(Client('opc.tcp://192.168.0.52:4840'))
-clients.append(Client('opc.tcp://192.168.0.55:4822'))
-
-print(clients)
